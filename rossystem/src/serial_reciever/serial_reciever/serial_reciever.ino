09931e938bfe6e8a2efa9fe2f0c7df99037b0f55
Zohar Cochavi <zohar.cochavi@gmail.com>
2020-01-31 11:22:05 +0100

    Wireless serial communication
    Adds support for wireless (one-way) serial communication using two TTGO ESP32 SX1276 LoRa microcontrollers

---
+++ rossystem/src/serial_reciever/serial_reciever/serial_reciever.ino
@@ -0,0 +1,62 @@
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/ttgo-lora32-sx1276-arduino-ide/
+*********/
+
+//Libraries for LoRa
+#include <SPI.h>
+#include <LoRa.h>
+
+//define the pins used by the LoRa transceiver module
+#define SCK 5
+#define MISO 19
+#define MOSI 27
+#define SS 18
+#define RST 14
+#define DIO0 26
+
+//433E6 for Asia
+//866E6 for Europe
+//915E6 for North America
+#define BAND 866E6
+
+String LoRaData;
+
+void setup() {   
+  //initialize Serial Monitor
+  Serial.begin(115200);
+
+  Serial.println("LoRa Receiver Test");
+  
+  //SPI LoRa pins
+  SPI.begin(SCK, MISO, MOSI, SS);
+  //setup LoRa transceiver module
+  LoRa.setPins(SS, RST, DIO0);
+
+  if (!LoRa.begin(BAND)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  Serial.println("LoRa Initializing OK!");
+}
+
+void loop() {
+
+  //try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    //received a packet
+    Serial.print("Received packet ");
+
+    //read packet
+    while (LoRa.available()) {
+      LoRaData = LoRa.readString();
+      Serial.print(LoRaData);
+    }
+
+    //print RSSI of packet
+    int rssi = LoRa.packetRssi();
+    Serial.print(" with RSSI ");    
+    Serial.println(rssi);
+  }
+}
---
+++ rossystem/src/serial_transmitter/serial_transmitter/serial_transmitter.ino
@@ -0,0 +1,58 @@
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/ttgo-lora32-sx1276-arduino-ide/
+*********/
+
+//Libraries for LoRa
+#include <SPI.h>
+#include <LoRa.h>
+
+//define the pins used by the LoRa transceiver module
+#define SCK 5
+#define MISO 19
+#define MOSI 27
+#define SS 18
+#define RST 14
+#define DIO0 26
+
+//433E6 for Asia
+//866E6 for Europe
+//915E6 for North America
+#define BAND 866E6
+
+//packet counter
+int counter = 0;
+
+void setup() {  
+  //initialize Serial Monitor
+  Serial.begin(115200);
+  
+  Serial.println("LoRa Sender Test");
+
+  //SPI LoRa pins
+  SPI.begin(SCK, MISO, MOSI, SS);
+  //setup LoRa transceiver module
+  LoRa.setPins(SS, RST, DIO0);
+  
+  if (!LoRa.begin(BAND)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  Serial.println("LoRa Initializing OK!");
+  delay(2000);
+}
+
+void loop() {
+   
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
+
+  //Send LoRa packet to receiver
+  LoRa.beginPacket();
+  LoRa.print("hello ");
+  LoRa.print(counter);
+  LoRa.endPacket();
+  counter++;
+  
+  delay(1000);
+}
