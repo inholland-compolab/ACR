56b65452d2b00cfe688b5b190be83313befbcecc
Zohar Cochavi <zohar.cochavi@gmail.com>
2020-03-07 20:39:12 +0100

    Updates arduino code
    Makes arduino control motor movement/speed via timer registers, making it able to continuously recieve ros messages, regardless of motors running.

--- arduino_src/arduino_src.ino
+++ arduino_src/arduino_src.ino
@@ -1,5 +1,4 @@
-//Controlling arduino motors using ROS Twist keyboard
-//rosrun rosserial_python serial_node.py /dev/ttyUSB0
+//Controlling arduino motors using ROS Twist keyboard rosrun rosserial_python serial_node.py /dev/ttyUSB0
 //rosrun teleop_twist_keyboard teleop_twist_keyboard.py
 
 #include <stdlib.h>
@@ -7,7 +6,6 @@
 #include <geometry_msgs/Twist.h>
 
 #define N_MOTORS 4
-#define N_STEPS 5000
 
 ros::NodeHandle  nh;
 geometry_msgs::Twist msg;
@@ -18,14 +16,13 @@ float move2;
 const int ena[] = {46, 47, 44, 45};
 const int otherEnable[] = {4, 5, 6, 7};
 const int dir[] = {50, 51, 48, 49};
-const int motionEna[] = {8, 9, 10, 11};
+const int motionEna[] = {9, 10, 11, 12};
 
-// TODO Use fast PWM mode on a 256 prescaler with the settings register
-// 	'Toggle OCRnA on compare match' to generate a pwm signal at 50%
-// 	duty cycle with a variable frequency (122 - 31250Hz)
-
+// Is called when a new twist message is published on the given topic
 void twist_callback(const geometry_msgs::Twist& cmd_vel)
 {  
+	die();
+
 	move1 = cmd_vel.linear.x;
 	move2 = cmd_vel.angular.z;
 	
@@ -46,113 +43,110 @@ void twist_callback(const geometry_msgs::Twist& cmd_vel)
 	}
 	else if (move1 < 0 && move2 < 0) {
 		turnright();
+	} else {
+		return;
 	}
-	else {
-		die();
-	}
+	live();
+}
+
+// Is currently in PWM mode, just because it's easy to set up, varying
+// the duty cycle will have no effect on the motors, the prescaler will
+void motor_pwm_setup() {
+	// Timer 1 PWM mode 64 prescaler
+	TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(WGM11) | _BV(WGM10);
+	TCCR1B = _BV(CS11);
+	OCR1A = 180;
+	OCR1B = 180;
+
+	// Timer 2 PWM mode 64 prescaler
+	TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
+	TCCR2B = _BV(CS22);
+	OCR2A = 180;
+	OCR2B = 180;
 }
 
 ros::Subscriber <geometry_msgs::Twist> sub("/cmd_vel", twist_callback);
 
 void setup() {
+	// Set up ROS node communication
 	nh.initNode();
 	nh.subscribe(sub);
-	
+
+	// Set up pins
 	for (int i=0; i<N_MOTORS; i++) {
 		pinMode(ena[i], OUTPUT);   
 		pinMode(dir[i], OUTPUT);
 		pinMode(motionEna[i], OUTPUT);
 		pinMode(otherEnable[i], OUTPUT);
 		
-		digitalWrite(motionEna[i], LOW);
 		digitalWrite(otherEnable[i], LOW);
 		digitalWrite(dir[i], LOW);
 		digitalWrite(ena[i], LOW); //LOW == Active
 	} 
+	die();                // Kill the motors
+	motor_pwm_setup();    // and start the motor clock signals
 }
 
-void loop() {   
-	nh.spinOnce();
-	delay(100);
+void loop() {
+	nh.spinOnce();    // Check ROS for any incoming messages
 }
 
-void front() {        
-	// Set wheel directions
-	digitalWrite(dir[0], LOW);
-	digitalWrite(dir[1], HIGH);
-	digitalWrite(dir[2], LOW);
-	digitalWrite(dir[3], HIGH);
-	
-	move();
-}
-
-void back() {
-  // Set wheel directions
-	digitalWrite(dir[0], HIGH);
-	digitalWrite(dir[1], LOW);
-	digitalWrite(dir[2], HIGH);
-	digitalWrite(dir[3], LOW);
-
-	move();
-}
-
-void left() {
-  // Set wheel directions    
-	digitalWrite(dir[0], HIGH);
-	digitalWrite(dir[1], HIGH);
-	digitalWrite(dir[2], LOW);
-	digitalWrite(dir[3], LOW);
-
-	move();
-}
-
-void right() {
-  // Set wheel directions     
-	digitalWrite(dir[0], LOW);
-	digitalWrite(dir[1], LOW);
-	digitalWrite(dir[2], HIGH);
-	digitalWrite(dir[3], HIGH);
-	
-	move();
-}
-
-void turnleft() {
-  // Set wheel directions
-	digitalWrite(dir[0], LOW);
-	digitalWrite(dir[1], LOW);
-	digitalWrite(dir[2], LOW);
-	digitalWrite(dir[3], LOW);
-
-	move();
-}
-
-void turnright() {
-  // Set wheel directions
-	digitalWrite(dir[0], HIGH);
-	digitalWrite(dir[1], HIGH);
-	digitalWrite(dir[2], HIGH);
-	digitalWrite(dir[3], HIGH);
-
-	move();
-}
-
+// Kill all motors
 void die() {
 	for (int i=0; i<N_MOTORS; ++i) {
 		digitalWrite(motionEna[i] , HIGH);
 	}
 }
 
-void move() {
-	for(int j=0; j<N_STEPS; ++j){
-		for(int i=0; i<N_MOTORS; ++i) {
-			digitalWrite(motionEna[i], HIGH);
-		}
-
-		delay(1);
-    
-		for(int i=0; i<N_MOTORS; ++i) {
-			digitalWrite(motionEna[i], LOW);
-		}
-    		delay(1);
+// Set all motors on
+void live() {
+	for (int i=0; i<N_MOTORS; ++i) {
+		digitalWrite(motionEna[i] , LOW);
 	}
 }
+
+// The following fucntions set set the stepper controllers
+// to turn the motors in the given direction
+// NOTE: left and right are strafing directions
+void front() {        
+	digitalWrite(dir[0], LOW);
+	digitalWrite(dir[1], HIGH);
+	digitalWrite(dir[2], LOW);
+	digitalWrite(dir[3], HIGH);
+}
+
+void back() {
+	digitalWrite(dir[0], HIGH);
+	digitalWrite(dir[1], LOW);
+	digitalWrite(dir[2], HIGH);
+	digitalWrite(dir[3], LOW);
+}
+
+void left() {
+	digitalWrite(dir[0], HIGH);
+	digitalWrite(dir[1], HIGH);
+	digitalWrite(dir[2], LOW);
+	digitalWrite(dir[3], LOW);
+}
+
+void right() {
+	digitalWrite(dir[0], LOW);
+	digitalWrite(dir[1], LOW);
+	digitalWrite(dir[2], HIGH);
+	digitalWrite(dir[3], HIGH);
+}
+
+void turnleft() {
+	digitalWrite(dir[0], LOW);
+	digitalWrite(dir[1], LOW);
+	digitalWrite(dir[2], LOW);
+	digitalWrite(dir[3], LOW);
+}
+
+void turnright() {
+	digitalWrite(dir[0], HIGH);
+	digitalWrite(dir[1], HIGH);
+	digitalWrite(dir[2], HIGH);
+	digitalWrite(dir[3], HIGH);
+}
+
